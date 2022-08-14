75af4f9e2788b2e2cbaafcbd39d6b5a21fefb7a9
Andy Webster <andywebster.online@gmail.com>
2022-08-14 10:11:18 +0100

    Change posts logic to build time instead of on page request

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,33 +1,22 @@
 import fs from 'fs'
 import { join } from 'path'
-import { useEffect } from 'react'
 import Preview from '../components/preview'
-import getConfig from 'next/config'
-const { serverRuntimeConfig } = getConfig()
 
-const POSTS_PATH = join(
-  serverRuntimeConfig.PROJECT_ROOT || process.cwd(),
-  'content/posts'
-)
+const POSTS_PATH = join(process.cwd(), 'content/posts')
 
 interface PostsProps {
   posts: any
   page?: number
   collection?: string
-  err?: string
 }
 export default function Posts(props: PostsProps) {
-  const { collection, page, posts, err } = props
-
-  useEffect(() => {
-    if (err) {
-      console.log(err)
-    }
-  }, [err])
+  const { collection, page, posts } = props
   return (
     <>
       <div className='container max-w-3xl my-10 px-6'>
-        <h1 className='font-bold font-serif text-5xl mb-10'>Posts</h1>
+        <h1 className='font-bold font-serif text-5xl mb-10'>
+          {collection ? collection : 'Posts'}
+        </h1>
       </div>
       <div className='container max-w-3xl my-10 px-6'>
         <ul className=''>{!!posts && posts.map(Preview)}</ul>
@@ -37,55 +26,44 @@ export default function Posts(props: PostsProps) {
   )
 }
 
-export async function getServerSideProps(context: any) {
+export async function getStaticProps(context: any) {
   const { query } = context
   const collection = query?.collection || null
   const page = query?.page || 1
   const postsPerPage = 10
 
-  try {
-    // Get post paths
-    const paths: any = fs
-      .readdirSync(POSTS_PATH)
-      // Remove file extensions for page paths
-      .map((path) => path.replace(/\.mdx?$/, ''))
-      //   Map the path into the static paths object required by Next.js
-      .map((slug) => ({ params: { slug } }))
-    // Load post front matter attributes from paths
-    const posts = paths
-      .map(({ params }: { params: any }) => {
-        const { slug } = params
-        const markdown = require(`/content/posts/${slug}.md`)
-        const { attributes } = markdown
-        return { slug, attributes }
-      })
-      .filter((post: any, index: number) => {
-        if (!collection) {
-          return true
-        } else {
-          const { slug, attributes } = post
-          return attributes.collection === collection
-        }
-      })
-
-    // Sort by most recent
-    posts.sort((itemA: any, itemB: any) => {
-      const dateA = new Date(itemA.attributes.date).getTime()
-      const dateB = new Date(itemB.attributes.date).getTime()
-      return dateB - dateA
+  // Get post paths
+  const paths: any = fs
+    .readdirSync(POSTS_PATH)
+    // Remove file extensions for page paths
+    .map((path) => path.replace(/\.mdx?$/, ''))
+    //   Map the path into the static paths object required by Next.js
+    .map((slug) => ({ params: { slug } }))
+  // Load post front matter attributes from paths
+  const posts = paths
+    .map(({ params }: { params: any }) => {
+      const { slug } = params
+      const markdown = require(`/content/posts/${slug}.md`)
+      const { attributes } = markdown
+      return { slug, attributes }
+    })
+    .filter((post: any, index: number) => {
+      if (!collection) {
+        return true
+      } else {
+        const { slug, attributes } = post
+        return attributes.collection === collection
+      }
     })
 
-    return {
-      props: { posts, collection, page }, // will be passed to the page component as props
-    }
-  } catch (err) {
-    return {
-      props: {
-        posts: [],
-        collection: null,
-        page: 1,
-        err: err,
-      },
-    }
+  // Sort by most recent
+  posts.sort((itemA: any, itemB: any) => {
+    const dateA = new Date(itemA.attributes.date).getTime()
+    const dateB = new Date(itemB.attributes.date).getTime()
+    return dateB - dateA
+  })
+
+  return {
+    props: { posts, collection, page }, // will be passed to the page component as props
   }
 }
