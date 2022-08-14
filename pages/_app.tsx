be47c5bc9286624054f73ee9c3ab605f47b9276b
Andy Webster <andywebster.online@gmail.com>
2022-08-14 09:56:45 +0100

    Attempt to log posts error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
-import ErrorBoundary from '../components/ErrorBoundary'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ErrorBoundary>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ErrorBoundary>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   )
 }
 
--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import { join } from 'path'
+import { useEffect } from 'react'
 import Preview from '../components/preview'
 
 const POSTS_PATH = join(process.cwd(), 'content/posts')
@@ -8,15 +9,20 @@ interface PostsProps {
   posts: any
   page?: number
   collection?: string
+  err?: string
 }
 export default function Posts(props: PostsProps) {
-  const { collection, page, posts } = props
+  const { collection, page, posts, err } = props
+
+  useEffect(() => {
+    if (err) {
+      console.log(err)
+    }
+  }, [err])
   return (
     <>
       <div className='container max-w-3xl my-10 px-6'>
-        <h1 className='font-bold font-serif text-5xl mb-10'>
-          {collection ? collection : 'Posts'}
-        </h1>
+        <h1 className='font-bold font-serif text-5xl mb-10'>Posts</h1>
       </div>
       <div className='container max-w-3xl my-10 px-6'>
         <ul className=''>{!!posts && posts.map(Preview)}</ul>
@@ -32,38 +38,49 @@ export async function getServerSideProps(context: any) {
   const page = query?.page || 1
   const postsPerPage = 10
 
-  // Get post paths
-  const paths: any = fs
-    .readdirSync(POSTS_PATH)
-    // Remove file extensions for page paths
-    .map((path) => path.replace(/\.mdx?$/, ''))
-    //   Map the path into the static paths object required by Next.js
-    .map((slug) => ({ params: { slug } }))
-  // Load post front matter attributes from paths
-  const posts = paths
-    .map(({ params }: { params: any }) => {
-      const { slug } = params
-      const markdown = require(`/content/posts/${slug}.md`)
-      const { attributes } = markdown
-      return { slug, attributes }
-    })
-    .filter((post: any, index: number) => {
-      if (!collection) {
-        return true
-      } else {
-        const { slug, attributes } = post
-        return attributes.collection === collection
-      }
+  try {
+    // Get post paths
+    const paths: any = fs
+      .readdirSync(POSTS_PATH)
+      // Remove file extensions for page paths
+      .map((path) => path.replace(/\.mdx?$/, ''))
+      //   Map the path into the static paths object required by Next.js
+      .map((slug) => ({ params: { slug } }))
+    // Load post front matter attributes from paths
+    const posts = paths
+      .map(({ params }: { params: any }) => {
+        const { slug } = params
+        const markdown = require(`/content/posts/${slug}.md`)
+        const { attributes } = markdown
+        return { slug, attributes }
+      })
+      .filter((post: any, index: number) => {
+        if (!collection) {
+          return true
+        } else {
+          const { slug, attributes } = post
+          return attributes.collection === collection
+        }
+      })
+
+    // Sort by most recent
+    posts.sort((itemA: any, itemB: any) => {
+      const dateA = new Date(itemA.attributes.date).getTime()
+      const dateB = new Date(itemB.attributes.date).getTime()
+      return dateB - dateA
     })
 
-  // Sort by most recent
-  posts.sort((itemA: any, itemB: any) => {
-    const dateA = new Date(itemA.attributes.date).getTime()
-    const dateB = new Date(itemB.attributes.date).getTime()
-    return dateB - dateA
-  })
-
-  return {
-    props: { posts, collection, page }, // will be passed to the page component as props
+    return {
+      props: { posts, collection, page }, // will be passed to the page component as props
+    }
+  } catch (err) {
+    return {
+      props: {
+        posts: [],
+        collection: null,
+        page: 1,
+        err: err,
+      },
+    }
   }
 }
